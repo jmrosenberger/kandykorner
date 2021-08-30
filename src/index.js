efe159603856e4280bf5645d8c14764c410fa607
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-08-30 01:39:11 -0500

    framework

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import KandyKorner from 'KandyKorner.js';
+import { KandyKorner } from './components/KandyKorner.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
