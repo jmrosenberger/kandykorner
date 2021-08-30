a17d33f6879645d5340ba64e5891ab7c29dc6ba1
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-08-30 02:34:01 -0500

    product list build

---
+++ src/components/locations/LocationList.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react"
+
+export const LocationList = () => {
+    const [locations, setLocations] = useState([])
+
+
+    useEffect(
+        () => {
+            fetch("http://localhost:3749/locations")
+            .then(response => response.json())
+            .then((data) => {
+                setLocations(data)
+            })
+        },
+        []
+    )
+
+    return (
+        <>
+        
+        {
+            locations.map(
+                (location) => {
+                    return <h4 key={`location--${location.id}`}>{location.address}</h4>
+                }
+            )
+        }
+        </>
+    )
+}
